938158ed94f1a2342ad5f9ab10c84a20c1394807
cbonoz <chrisdistrict@gmail.com>
2020-06-30 20:39:39 -0400

    update trust certificate and gitignore

--- ContentBrowser/src/main/java/com/amazon/android/contentbrowser/app/ContentBrowserApplication.java
+++ ContentBrowser/src/main/java/com/amazon/android/contentbrowser/app/ContentBrowserApplication.java
@@ -42,6 +42,16 @@ import android.view.WindowManager;
 import com.amazon.analytics.AnalyticsManager;
 import com.amazon.analytics.IAnalytics;
 
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.X509TrustManager;
+
 /**
  * Content browser application class.
  */
@@ -98,6 +108,30 @@ public class ContentBrowserApplication extends ModularApplication {
         return ContentBrowserApplication.getInstance().mRefWatcher;
     }
 
+
+    public static void trustEveryone() {
+        try {
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier(){
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }});
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, new X509TrustManager[]{new X509TrustManager(){
+                public void checkClientTrusted(X509Certificate[] chain,
+                                               String authType) throws CertificateException {}
+                public void checkServerTrusted(X509Certificate[] chain,
+                                               String authType) throws CertificateException {}
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }}}, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(
+                    context.getSocketFactory());
+        } catch (Exception e) { // should never happen
+            e.printStackTrace();
+        }
+    }
+
+
     /**
      * {@inheritDoc}
      */
@@ -107,7 +141,7 @@ public class ContentBrowserApplication extends ModularApplication {
         super.onCreate();
 
         sInstance = (ContentBrowserApplication) getApplicationContext();
-
+        trustEveryone(); // Needed for trust anchor temporary bypass.
         Preferences.setContext(this);
 
         mAnalyticsManager = AnalyticsManager.getInstance(this);
